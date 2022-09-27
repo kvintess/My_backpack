c93c7d7c69cd1bd07eb099c43f305eba54c9e3b2
kvintessentsia <trinitikvintessentsia@yandex.ru>
2022-09-27 17:21:09 +0300

    let go!

--- MyBackpack.py
+++ MyBackpack.py
@@ -9,7 +9,35 @@ class Backpack:
         print('тут лежит:')
         for item in self.content:
             print('  ',item)
-my_backpack= Backpack()
-my_backpack.add('зарядка')
-my_backpack.add('ноут')
-my_backpack.inspect()
+    def __str__(self):
+        return 'backpack:' + ','.join(self.content)
+
+    def __bool__(self):
+        return self.content != []
+
+    def __eq__(self, other):
+        return self.content != other.content
+
+    def __add__(self, other):
+        new_obj = Backpack()
+        new_obj.content.extend(self.content)
+        if isinstance(other, Backpack):
+            new_obj.content.extend(other.content)
+        else:
+            new_obj.content.extend(other)
+        return new_obj
+
+#окей идем дальше
+#расширенное присвоение += -= и т.д
+    def __iadd__(self, other):
+        self.content.extend(other.content)
+        return self
+
+my_backpack = Backpack()
+my_backpack.add('tetrad')
+my_son_backpack = Backpack()
+my_son_backpack.add('ruchka')
+my_backpack += my_son_backpack
+print(my_backpack)
+
+
